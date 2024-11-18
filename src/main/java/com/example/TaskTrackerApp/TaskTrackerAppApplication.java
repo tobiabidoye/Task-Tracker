6b935de24bd797ab03ce0853ba4ffbab12e83ca8
Tobi Abidoye <tobiabidoye14@gmail.com>
2024-11-17 22:06:14 -0500

    Initial commit

---
+++ src/main/java/com/example/TaskTrackerApp/TaskTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskTrackerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerAppApplication.class, args);
+	}
+
+}
